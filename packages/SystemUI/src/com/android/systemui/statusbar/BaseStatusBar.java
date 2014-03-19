de5860a10b747a826e4b4ace984c730b6cbce157
AragaoAnderson <andersonaragao@me.com>
2014-03-19 01:06:41 -0300

    Fix Slim Recents changes
    Change-Id: Ic9b88bda08e9ff73f28ae0ed6952f8b6c2292f57

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -360,15 +360,13 @@ public abstract class BaseStatusBar extends SystemUI implements
                         mContext.getContentResolver(), Settings.System.CUSTOM_RECENT, false);
 
         if(mCustomRecent){
-            cRecents = new RecentController(mContext);
+            cRecents = new RecentController(mContext, mLayoutDirection);
         }else{
             mRecents = getComponent(RecentsComponent.class);
         }
 
         mLocale = mContext.getResources().getConfiguration().locale;
         mLayoutDirection = TextUtils.getLayoutDirectionFromLocale(mLocale);
-
-        mRecents = new RecentController(mContext, mLayoutDirection);
 
         mStatusBarContainer = new FrameLayout(mContext);
 
