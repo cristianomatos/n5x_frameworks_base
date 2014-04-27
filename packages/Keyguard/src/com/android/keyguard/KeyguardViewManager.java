db1dde9a9b48ccc4d44dc92d56b0bf9d0c101e03
AragaoAnderson <andersonaragao@me.com>
2014-04-27 13:55:34 -0300

    Keyguard: LN: fix and harden against null pointer exceptions
    Change-Id: Icd726bd3acb91e49b621ce7f920364dc9d7177db

--- packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
@@ -139,6 +139,8 @@ public class KeyguardViewManager {
             ContentResolver resolver = mContext.getContentResolver();
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.LOCKSCREEN_NOTIFICATIONS), false, this);
+
+            updateSettings();
         }
 
         @Override
@@ -699,10 +701,12 @@ public class KeyguardViewManager {
                 options.getBoolean(IS_SWITCHING_USER));
 
         if (mLockscreenNotifications) {
-            mNotificationView = (NotificationHostView)mKeyguardView.findViewById(R.id.notification_host_view);
-            mNotificationViewManager.setHostView(mNotificationView);
-            mNotificationViewManager.onScreenTurnedOff();
-            mNotificationView.addNotifications();
+            mNotificationView = (NotificationHostView) mKeyguardView.findViewById(R.id.notification_host_view);
+            if (mNotificationViewManager != null && mNotificationView != null) {
+                mNotificationViewManager.setHostView(mNotificationView);
+                mNotificationViewManager.onScreenTurnedOff();
+                mNotificationView.addNotifications();
+            }
         }
 
         // HACK
@@ -807,7 +811,7 @@ public class KeyguardViewManager {
         if (mKeyguardView != null) {
             mKeyguardView.onScreenTurnedOff();
         }
-        if (mLockscreenNotifications) {
+        if (mNotificationViewManager != null) {
             mNotificationViewManager.onScreenTurnedOff();
         }
     }
@@ -859,7 +863,9 @@ public class KeyguardViewManager {
         }
 
         if (mLockscreenNotifications) {
-            mNotificationViewManager.onScreenTurnedOn();
+            if (mNotificationViewManager != null) {
+                mNotificationViewManager.onScreenTurnedOn();
+            }
         }
     }
 
@@ -876,7 +882,9 @@ public class KeyguardViewManager {
         if (DEBUG) Log.d(TAG, "hide()");
 
         if (mLockscreenNotifications) {
-            mNotificationViewManager.onDismiss();
+            if (mNotificationViewManager != null) {
+                mNotificationViewManager.onDismiss();
+            }
         }
 
         if (mKeyguardHost != null) {
