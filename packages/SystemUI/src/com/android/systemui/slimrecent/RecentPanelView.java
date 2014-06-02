abd16667bac96f2fbb5b4e69c31b99ea827c9fef
AragaoAnderson <andersonaragao@me.com>
2014-06-02 13:52:12 -0300

    Framework: Fix for latest 2 commits
    Change-Id: I9966e07fc81da84f1948dc4a27f0a52c9677cdb8

--- packages/SystemUI/src/com/android/systemui/slimrecent/RecentPanelView.java
+++ packages/SystemUI/src/com/android/systemui/slimrecent/RecentPanelView.java
@@ -20,6 +20,7 @@ package com.android.systemui.slimrecent;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.ActivityOptions;
+import android.app.INotificationManager;
 import android.app.TaskStackBuilder;
 import android.app.admin.DevicePolicyManager;
 import android.content.ActivityNotFoundException;
@@ -37,6 +38,7 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Process;
+import android.os.ServiceManager;
 import android.os.UserHandle;
 import android.provider.Settings;
 import android.util.Log;
@@ -48,6 +50,7 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import com.android.cards.internal.Card;
 import com.android.cards.internal.CardArrayAdapter;
@@ -103,6 +106,8 @@ public class RecentPanelView {
     private final ImageView mEmptyRecentView;
 
     private final RecentController mController;
+
+    private INotificationManager mNotificationManager;
 
     // Our array adapter holding all cards
     private CardArrayAdapter mCardArrayAdapter;
@@ -290,6 +295,11 @@ public class RecentPanelView {
         // Force theme change to choose custom defined menu layout.
         final Context layoutContext = new ContextThemeWrapper(mContext, R.style.RecentBaseStyle);
 
+        if (mNotificationManager == null) {
+            mNotificationManager = INotificationManager.Stub.asInterface(
+                    ServiceManager.getService(Context.NOTIFICATION_SERVICE));
+        }
+
         final PopupMenu popup = new PopupMenu(layoutContext, selectedView, Gravity.RIGHT);
         mPopup = popup;
 
@@ -341,11 +351,15 @@ public class RecentPanelView {
                 if (item.getItemId() == MENU_APP_DETAILS_ID) {
                     startApplicationDetailsActivity(td.packageName, null, null);
                 } else if (item.getItemId() == MENU_APP_FLOATING_ID) {
-                    Intent transparent = new Intent(mContext, com.android.systemui.Transparent.class);
-                    transparent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
-                            | Intent.FLAG_FLOATING_WINDOW
-                            | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
-                    mContext.startActivity(transparent);
+                String currentViewPackage = td.packageName;
+                boolean allowed = true;
+                try {
+                    // preloaded apps are added to the blacklist array when is recreated, handled in the notification manager
+                    allowed = mNotificationManager.isPackageAllowedForFloatingMode(currentViewPackage);
+                } catch (android.os.RemoteException ex) {
+                // System is dead
+                }
+                if (allowed) {
                     Intent intent = new Intent(Intent.ACTION_MAIN);
                     intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
                             | Intent.FLAG_ACTIVITY_CLEAR_TASK
@@ -353,6 +367,11 @@ public class RecentPanelView {
                     intent.setComponent(td.intent.getComponent());
                     mContext.startActivity(intent);
                     exit();
+                } else {
+                    String text = mContext.getResources().getString(R.string.floating_mode_blacklisted_app);
+                    int duration = Toast.LENGTH_LONG;
+                    Toast.makeText(mContext, text, duration).show();
+                }
                 } else if (item.getItemId() == MENU_APP_STOP_ID) {
                     ActivityManager am = (ActivityManager)mContext.getSystemService(
                             Context.ACTIVITY_SERVICE);
