234bbe1d3f217ec3f245bb5b37fc7a3dc6c77b3f
Schubi <schubi@erlangen.ccc.de>
2014-06-17 10:17:54 -0300

    HOVER: Option to disable HOVER-Button in StatusBar [1/2] If the button is hidden, user can activate Hover in the Settings instead
    Change-Id: I238352327ff8b0114dff70bbdeab98aede996789

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -261,6 +261,7 @@ public abstract class BaseStatusBar extends SystemUI implements
     //HOVER TWEAKS
     protected boolean mHoverEnabled;
     protected boolean mHoverActive;
+    protected boolean mHoverHideButton;
     protected ImageView mHoverButton;
     protected HoverCling mHoverCling;
 
@@ -561,6 +562,16 @@ public abstract class BaseStatusBar extends SystemUI implements
             }
         });
 
+        // Listen for HOVER button override
+        mContext.getContentResolver().registerContentObserver(
+                Settings.System.getUriFor(Settings.System.HOVER_HIDE_BUTTON),
+                        false, new ContentObserver(new Handler()) {
+            @Override
+            public void onChange(boolean selfChange) {
+                updateHoverActive();
+            }
+        });
+
         //updateHoverState();
         updateHoverActive();
 
@@ -704,7 +715,7 @@ public abstract class BaseStatusBar extends SystemUI implements
     }*/
 
     protected void updateHoverButton(boolean shouldBeVisible) {
-        mHoverButton.setVisibility((mHoverEnabled && shouldBeVisible) ? View.VISIBLE : View.GONE);
+        mHoverButton.setVisibility((mHoverEnabled && !mHoverHideButton && shouldBeVisible) ? View.VISIBLE : View.GONE);
     }
 
     protected void updateHoverButton() {
@@ -718,6 +729,9 @@ public abstract class BaseStatusBar extends SystemUI implements
         mHoverActive = mHoverEnabled &&
                 Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.HOVER_ACTIVE, 0) == 1;
+
+        mHoverHideButton = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.HOVER_HIDE_BUTTON, 0) == 1;
 
         updateHoverButton();
         if (mHoverEnabled) {
